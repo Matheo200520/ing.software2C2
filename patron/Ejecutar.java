3b37a81c3bfadac92e077f033df871847fe0a2e5
Matheo200520 <lcmatheo20@gmail.com>
2025-10-17 17:09:16 +0000

    se aplica bien el concepto de herncia correctamente

--- patron/Ejecutar.java
+++ patron/Ejecutar.java
@@ -1,66 +1,73 @@
 public class Ejecutar {
     public static void main(String[] args) {
+
+        // Crear clientes
         Cliente cliente1 = new Cliente("3151267888", "Matheo López");
         Cliente cliente2 = new Cliente("3178502783", "Danny Casso");
 
+        // Crear pedidos
         Pedido pedido = new Pedido(cliente1, 22331458);
         Pedido pedido2 = new Pedido(cliente2, 10346547);
 
-        // Crear impresiones usando Factory Method
-        Impresion impresionColor = Impresion.crearImpresion(1, "Impresión de fotos familiares", 1500, "color");
-        Impresion impresionBN = Impresion.crearImpresion(2, "Impresión de documentos", 500, "blanco y negro");
-        Camara camara = new Camara(3, "Cámara profesional para fotos con una resolucion 2k", 12000, "Canon", "EOS 90D");
+        // Crear productos del cliente 1 (usando la clase base Producto)
+        Producto impresionColor = Impresion.crearImpresion(1, "Impresión de fotos familiares", 1500, "color");
+        Producto impresionBN = Impresion.crearImpresion(2, "Impresión de documentos", 500, "blanco y negro");
+        Producto camara = new Camara(3, "Cámara profesional para fotos con una resolución 2k", 12000, "Canon", "EOS 90D");
 
+        // Crear productos del cliente 2
+        Producto impresionColor2 = Impresion.crearImpresion(4, "Impresión de fotos familiares", 1500, "color");
+        Producto impresionbyn = Impresion.crearImpresion(5, "Impresión de documentos", 500, "blanco y negro");
+        Producto impresionbyn2 = Impresion.crearImpresion(6, "Impresión de tablas de multiplicar", 700, "blanco y negro");
+        Producto camara2 = new Camara(7, "Cámara profesional multiuso de alta resolución", 15000, "Sony", "Alpha 1 II");
 
-        Impresion impresionColor2 = Impresion.crearImpresion(1, "Impresión de fotos familiares", 1500, "color");
-        Impresion impresionbyn = Impresion.crearImpresion(2, "Impresión de documentos", 500, "blanco y negro");
-        Impresion impresionbyn2 = Impresion.crearImpresion(3, "Impresión de tablas de multiplicar", 700, "blanco y negro");
-        Camara camara2 = new Camara(4, "Cámara profesional para fotos y de documentos con su modo multiuso de alta resolucion", 15000, "Sony", "Alpha 1 II");
+        // Convertir a tipo concreto para usar métodos propios
+        Camara cam1 = (Camara) camara;
+        Camara cam2 = (Camara) camara2;
+        Impresion impColor = (Impresion) impresionColor;
+        Impresion impBN = (Impresion) impresionBN;
+        Impresion impColor2 = (Impresion) impresionColor2;
+        Impresion impBN1 = (Impresion) impresionbyn;
+        Impresion impBN2 = (Impresion) impresionbyn2;
 
+        // Tomar fotos y agregarlas (cliente 1)
+        String foto1 = cam1.tomarFoto();
+        String foto2 = cam1.tomarFoto();
+        impColor.agregarFoto(foto1);
+        impBN.agregarFoto(foto2);
 
-      // Tomar fotos y agregarlas
-        String foto1 = camara.tomarFoto();
-        String foto2 = camara.tomarFoto();
+        // Agregar productos al pedido del cliente 1
+        pedido.agregarProducto(impColor);
+        pedido.agregarProducto(impBN);
+        pedido.agregarProducto(cam1);
 
-        impresionColor.agregarFoto(foto1);
-        impresionBN.agregarFoto(foto2);
-
-
-        // Agregar productos al pedido
-        pedido.agregarProducto(impresionColor);
-        pedido.agregarProducto(impresionBN);
-        pedido.agregarProducto(camara);
-
-        // Mostrar pedido completo
+        // Mostrar pedido completo del cliente 1
         pedido.mostrarPedido();
 
-        // Imprimir fotos
-        impresionColor.imprimir();
-        impresionBN.imprimir();
+        // Imprimir fotos cliente 1
+        impColor.imprimir();
+        impBN.imprimir();
 
+        // Tomar fotos y agregarlas (cliente 2)
+        String foto3 = cam2.tomarFoto();
+        String foto4 = cam2.tomarFoto();
+        String foto5 = cam2.tomarFoto();
 
+        impColor2.agregarFoto(foto3);
+        impBN1.agregarFoto(foto4);
+        impBN2.agregarFoto(foto5);
 
-        String foto3 = camara.tomarFoto();
-        String foto4 = camara.tomarFoto();
-        String foto5 = camara.tomarFoto();
+        // Agregar productos al pedido del cliente 2
+        pedido2.agregarProducto(impColor2);
+        pedido2.agregarProducto(impBN1);
+        pedido2.agregarProducto(impBN2);
+        pedido2.agregarProducto(cam2);
 
-        impresionColor2.agregarFoto(foto3);
-        impresionbyn.agregarFoto(foto4);
-        impresionbyn2.agregarFoto(foto5);
-
-
-        pedido2.agregarProducto(impresionColor2);;
-        pedido2.agregarProducto(impresionbyn);
-        pedido2.agregarProducto(impresionbyn2);
-        pedido2.agregarProducto(camara2);
-
+        // Mostrar pedido completo del cliente 2
         pedido2.mostrarPedido();
 
-        impresionColor2.imprimir();
-        impresionbyn.imprimir();
-        impresionbyn2.imprimir();
-
-
-
+        // Imprimir fotos cliente 2
+        impColor2.imprimir();
+        impBN1.imprimir();
+        impBN2.imprimir();
     }
 }
